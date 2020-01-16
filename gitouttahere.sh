6fd7289b712320fc47131ee8fe55194b36ce11aa
Kade <m222868@usna.edu>
2020-01-16 13:11:24 -0500

    f

---
+++ gitouttahere.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script automatically creates a remote repository on my non-USNA
+# Github to allow me to acess stuff of of the yard. Which is awesome.
+
+curl -u kmheckel \
+       -H "Content-Type: application/json" \
+       -d '{"name":"'$1'"}' https://api.github.com/user/repos
+cd $1
+
+git remote add $1_public git@github.com:kmheckel/$1.git
+git push -u $1_public master
